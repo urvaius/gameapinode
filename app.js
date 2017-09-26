62a62cfd821fac7d0fc5e5e758d5b3673a06a7c2
Dave Arneson <urvaius@gmail.com>
2017-09-26 16:22:51 -0700

    setting up mongo db

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-var express = require ('express');
+var express = require ('express'),
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://urvaius:Buffy11$@arne-5-mongo1-shard-00-00-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-01-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-02-ujozx.mongodb.net:27017/bookAPI?ssl=true&replicaSet=arne-5-mongo1-shard-0&authSource=admin')
+
+var Book = require('./models/bookModel');
 
 var app = express();
 var port = process.env.PORT || 3000;
+ 
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req,res){
+        Book.find(function(err, books){
+            if(err)
+                console.og(err);
+            else
+                res.json(books);
+        });
+    });
+    
+app.use('/api', bookRouter);
 
 app.get('/', function(req, res){
     res.send('welcome to my api');
