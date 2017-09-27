ba16217dfdf55b82be887af25fa16529fe1f65c4
Dave Arneson <urvaius@gmail.com>
2017-09-27 13:53:59 -0700

    continuing on middleware

---
+++ Routes/bookRoutes.js
@@ -0,0 +1,55 @@
+var express = require('express');
+
+
+var routes = function(Book){
+    var bookRouter = express.Router();
+    
+    bookRouter.route('/')
+        .post(function(req,res){
+            var book = new Book(req.body);
+    
+            book.save();
+            res.status(201).send(book);
+    
+        })
+        .get(function(req,res){
+            var query = {};
+            if(req.query.genre)
+            {
+                query.genre = req.query.genre;
+            }
+            Book.find(query,function(err, books){
+                if(err)
+                    res.status(500).send(err);
+                else
+                    res.json(books);
+            });
+        });
+    
+    bookRouter.route('/:bookId')
+    .get(function(req,res){
+        
+        Book.findById(req.params.bookId,function(err, book){
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(book);
+        });
+    })
+    .put(function(req,res){
+        Book.findById(req.params.bookId, function(err, book){
+            if(err)
+                res.status(500).send(err);
+                else
+                    book.title = req.body.title;
+                    book.author = req.body.author;
+                    book.genre = req.body.genre;
+                    book.read = req.body.read;
+                    book.save();
+                    res.json(book);
+        });
+    });
+    return bookRouter;
+};
+
+module.exports = routes;
--- app.js
+++ app.js
@@ -1,42 +1,23 @@
 var express = require ('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
-var db = mongoose.connect('mongodb://urvaius:Buffy11$@arne-5-mongo1-shard-00-00-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-01-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-02-ujozx.mongodb.net:27017/bookAPI?ssl=true&replicaSet=arne-5-mongo1-shard-0&authSource=admin')
+
+var db = mongoose.connect('mongodb://urvaius:Buffy11$@arne-5-mongo1-shard-00-00-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-01-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-02-ujozx.mongodb.net:27017/bookAPI?ssl=true&replicaSet=arne-5-mongo1-shard-0&authSource=admin');
 
 var Book = require('./models/bookModel');
 
 var app = express();
 var port = process.env.PORT || 3000;
  
-var bookRouter = express.Router();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-bookRouter.route('/Books')
-    .get(function(req,res){
-        var query = {};
-        if(req.query.genre)
-        {
-            query.genre = req.query.genre;
-        }
-        Book.find(query,function(err, books){
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        });
-    });
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-bookRouter.route('/Books/:bookId')
-.get(function(req,res){
-    
-    Book.findById(req.params.bookId,function(err, book){
-        if(err)
-            res.status(500).send(err);
-        else
-            res.json(book);
-    });
-});
-app.use('/api', bookRouter);
 
+app.use('/api/books', bookRouter);
+//app.use('/api/authors', authorRouter);
 app.get('/', function(req, res){
     res.send('welcome to my api');
 });
